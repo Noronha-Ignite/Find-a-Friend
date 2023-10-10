430d44a0321534d9476b5e44716cc94331866e08
Gabriel Noronha <inc.691@gmail.com>
2023-10-10 16:05:04 -0300

    Validate if org email already exists on creation

--- src/services/create-org-service.spec.ts
+++ src/services/create-org-service.spec.ts
@@ -3,7 +3,9 @@ import { beforeEach, describe, expect, it } from 'vitest'
 
 import { InMemoryOrgRepository } from '../repositories/in-memory/in-memory-org-repository'
 import { OrgRepository } from '../repositories/org-repository'
+import { createOrganization } from '../utils/tests/create-organization'
 import { CreateOrgService } from './create-org-service'
+import { EmailAlreadyExistsError } from './errors/email-already-exists.error'
 
 let orgRepository: OrgRepository
 let sut: CreateOrgService
@@ -33,5 +35,24 @@ describe('Create organization service', async () => {
     )
 
     expect(organization).not.toHaveProperty('password_hash')
+  })
+
+  it('should not be able to create an organization with an existing email', async () => {
+    await createOrganization(orgRepository, {
+      email: 'existing@email.com',
+    })
+
+    await expect(() =>
+      sut.execute({
+        address: faker.location.streetAddress(),
+        cep: faker.location.zipCode(),
+        city: faker.location.city(),
+        email: 'existing@email.com',
+        latitude: faker.location.latitude(),
+        longitude: faker.location.longitude(),
+        password: faker.internet.password(),
+        whatsapp: faker.phone.number(),
+      }),
+    ).rejects.toBeInstanceOf(EmailAlreadyExistsError)
   })
 })
--- src/services/create-org-service.ts
+++ src/services/create-org-service.ts
@@ -1,11 +1,20 @@
 import { genSalt, hash } from 'bcryptjs'
 import { OrgRepository } from '../repositories/org-repository'
 import { sanitizeOrganization } from '../utils/sanitize-organization'
+import { EmailAlreadyExistsError } from './errors/email-already-exists.error'
 
 export class CreateOrgService {
   constructor(private orgRepository: OrgRepository) {}
 
   async execute({ password, ...params }: OrgCreateBody) {
+    const alreadyExistentOrgWithEmail = await this.orgRepository.findByEmail(
+      params.email,
+    )
+
+    if (alreadyExistentOrgWithEmail) {
+      throw new EmailAlreadyExistsError()
+    }
+
     const salt = await genSalt(6)
 
     const password_hash = await hash(password, salt)
