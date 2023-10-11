eb24ce02f6134e1d9f58b9f03662077ad2bc252a
Gabriel Noronha <inc.691@gmail.com>
2023-10-11 00:37:37 -0300

    Create fetch pets endpoint

---
+++ src/http/controllers/pets/fetch-pets.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeFetchPetsService } from '../../../services/factories/make-fetch-pets-service'
+
+export const fetchPets = async (req: FastifyRequest, reply: FastifyReply) => {
+  const fetchPetsParamsSchema = z.object({
+    city: z.string(),
+  })
+
+  const fetchPetsQuerySchema = z.object({
+    type: z.enum(['CAT', 'DOG']),
+    age: z.enum(['NEW_BORN', 'YOUNG', 'GROWN_UP']),
+    size: z.enum(['SMALLER', 'SMALL', 'MEDIUM', 'BIG']),
+    energyLevel: z.enum(['VERY_LOW', 'LOW', 'MEDIUM', 'HIGH', 'VERY_HIGH']),
+    independencyLevel: z.enum([
+      'VERY_LOW',
+      'LOW',
+      'MEDIUM',
+      'HIGH',
+      'VERY_HIGH',
+    ]),
+  })
+
+  const { city } = fetchPetsParamsSchema.parse(req.params)
+  const query = fetchPetsQuerySchema.parse(req.query)
+
+  const fetchPetService = makeFetchPetsService()
+
+  const { pets } = await fetchPetService.execute({
+    ...query,
+    city,
+  })
+
+  return reply.status(201).send({ pets })
+}
--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { fetchPets } from './fetch-pets'
 
 export const petsRoutes = async (app: FastifyInstance) => {
+  app.get('/pets/city/:city', fetchPets)
+
   app.post('/orgs/:orgId/pets', create)
 }
